b81f301bfa51341effd9a543c7093007287deaf0
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2021-05-11 16:30:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+"""File to be run to use Anime Atlas: Anime Recommender"""
+import frontend
+# Remember to run myapp.py to start the server for the API calls made in main.py
+# (Run myapp.py in console first, then run main.py while myapp.py is running)
+
+# Before any of this, make sure all certificates are working
+# You may need to reinstall them or switch to python 3.8
+
+# Moreover, in front end final make sure the token import statement is working,
+# you may have to import from the file directory
+
+frontend.main()
